629f0b71e953a619d082f3915f6e336b8c81ce65
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2021-09-16 07:32:48 +0600

    this concept complete

--- objector_this.js
+++ objector_this.js
@@ -1,8 +1,55 @@
+
+/*
+var obj={
+    name:'Mahadi Hasan',
+    print:function(){
+        console.log(this);
+    }
+}
+obj.print();*/
+
+// function myfun()
+// {
+//     //console.log(this);
+//     function inner(){
+//         console.log(this);
+//     }
+//    new inner();
+// }
+
+//    myfun();
+
+// var person={
+//     name:'Mahadi Hasan',
+//     print:function(){
+//         console.log(this);
+//         console.log('Hellow '+this.name );
+//     }
+// }
+// // var myname=person.name;
+// // console.log(myname);
+// var myfun=person.print.bind(person);
+// //console.log(myfun);
+// myfun();
+// function add(num){
+//    var result= this.value+num;
+//    console.log(result);
+// }
+// var obj={
+//     value:500,
+// }
+
+// // var result=add(40);
+// // console.log(result);
+// var binded=add.bind(obj);
+//  var result=binded(411);
+
 var person={
-    name:'Jahangir Kabir',
-    ocup:'Doctor',
-    print:function(){+
-        console.log(this.name+' '+this.ocup);
+    name:'Hellow Bangladesb',
+    print:function(){
+        setTimeout(function(){
+            console.log('Function called '+this.name); 
+        }.bind(this),5000)
     }
 }
 person.print();
